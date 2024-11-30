c00c7f495cd29f5f9ae3294a2cb3dda97cfe2f9f
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-11-30 22:13:12 +0600

    Class Students

---
+++ main.py
@@ -0,0 +1,10 @@
+# The class
+class student:
+    student_school = "Khulna Zilla School"
+    print("I am a student of Khulna Zilla School.")
+
+# The objects
+student1 = student()
+student2 = student()
+
+
